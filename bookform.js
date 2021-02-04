1d02f924d60aea55cf4114247fe0cea3efd5c566
Sirak <69489773+shizi5742@users.noreply.github.com>
2021-02-04 05:42:23 -0600

    final project

--- bookform.js
+++ bookform.js
@@ -7,7 +7,6 @@ function postData(event) {
 	let publisher = document.getElementById('publisher').value;
 	let published = document.getElementById('date-published').value;
 
-
 	fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/add", {
 		method: 'POST',
 		headers: { "Content-Type": "application/json" },
@@ -18,12 +17,26 @@ function postData(event) {
 	}).then((res) => res.json())
 		.then((data) => console.log(data))
 		.catch((err) => console.log(err))
+		
 }
 function save() {
 	document.getElementById("library-form").addEventListener("submit", postData)
+	success()
+
 }
+
 function myFunction() {
 	document.getElementById("library-form").reset()
 
+}
+function success() {
+	document.getElementById("show").innerHTML = `<div class="row">
+			<div class="col">
+					<div class="alert alert-success" role="alert">
+						 Book Successfully Added !
+					</div>
+			</div>
+	</div>`
+	// setTimeout(() => document.querySelector("#display").remove(), 2000)
 }
 
